3db357d394a77bbdbc93450bfd6c1fe0c06977e0
AdriMon27 <90414103+AdriMon27@users.noreply.github.com>
2022-05-28 18:57:55 +0200

    comentario

--- app/src/main/java/es/uji/al394516/aviajar/PresenterTE.kt
+++ app/src/main/java/es/uji/al394516/aviajar/PresenterTE.kt
@@ -4,6 +4,7 @@ import es.uji.al394516.aviajar.classes.Travel
 
 class PresenterTE(val view: ITravelEdition, val model: Model) {
     fun travelExists(currentTravel: Travel?) {
+        //si no tenemos viaje, activar edicion de viaje
         if (currentTravel == null){
             view.canUserModifyTravel(true)
             return
