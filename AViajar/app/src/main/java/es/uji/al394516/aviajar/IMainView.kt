a050bb521817ad0a354dccac39685c11689373a6
AdriMon27 <90414103+AdriMon27@users.noreply.github.com>
2022-08-15 00:53:37 +0200

    chachi pistachi
    Borrar la carpeta duplicada

--- AViajar/app/src/main/java/es/uji/al394516/aviajar/IMainView.kt
+++
@@ -1,8 +0,0 @@
-package es.uji.al394516.aviajar
-
-interface IMainView {
-    fun toNextActivity()
-    fun toTravelResults()
-    fun enableButton(enable:Boolean)
-    fun showMessage(message: String)
-}
